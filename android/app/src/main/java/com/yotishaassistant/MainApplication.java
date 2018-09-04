4612fad6b2d7dd4a1d6a4ffdfbc54aa401801ea1
Amir Reza <arg7971@gmail.com>
2018-09-04 16:37:43 +0430

    installed linear gradient package

--- android/app/src/main/java/com/yotishaassistant/MainApplication.java
+++ android/app/src/main/java/com/yotishaassistant/MainApplication.java
@@ -3,6 +3,7 @@ package com.yotishaassistant;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.horcrux.svg.SvgPackage;
 import io.realm.react.RealmReactPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new LinearGradientPackage(),
             new SvgPackage(),
             new RealmReactPackage(),
             new MapsPackage(),
